73f1716aa1b9ff945896560a8e771c514146231e
bdiaz28 <brittanymdiaz@yahoo.com>
2022-05-04 10:52:22 -0400

    finished

---
+++ build/precache-manifest.4c6d4a329ab6f31faf6f4e4c13601aed.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1305506c0a5cd768f355a750c8b121a5",
+    "url": "/FastTrackPortfolio/index.html"
+  },
+  {
+    "revision": "7c77ab8a30d437c840bc",
+    "url": "/FastTrackPortfolio/static/css/2.bf3e7d99.chunk.css"
+  },
+  {
+    "revision": "7c17a2d36fc5ca2aea98",
+    "url": "/FastTrackPortfolio/static/css/main.07996b5b.chunk.css"
+  },
+  {
+    "revision": "7c77ab8a30d437c840bc",
+    "url": "/FastTrackPortfolio/static/js/2.145b7d76.chunk.js"
+  },
+  {
+    "revision": "4b628ed9eccd239c62d6e29572810db8",
+    "url": "/FastTrackPortfolio/static/js/2.145b7d76.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7c17a2d36fc5ca2aea98",
+    "url": "/FastTrackPortfolio/static/js/main.93af9708.chunk.js"
+  },
+  {
+    "revision": "bf864af81b25124ab780",
+    "url": "/FastTrackPortfolio/static/js/runtime-main.3eef6aea.js"
+  },
+  {
+    "revision": "29b300451c1cd634d341bd0a1f40752d",
+    "url": "/FastTrackPortfolio/static/media/BGM1.29b30045.png"
+  },
+  {
+    "revision": "ec991c4f178d0c453f6713af9823887c",
+    "url": "/FastTrackPortfolio/static/media/BGM2.ec991c4f.png"
+  },
+  {
+    "revision": "3beb8a31788119a7684cb5ee156e9a96",
+    "url": "/FastTrackPortfolio/static/media/Resume_BDiaz.3beb8a31.pdf"
+  },
+  {
+    "revision": "22b33353f7cf3e64595af437bdc6bdad",
+    "url": "/FastTrackPortfolio/static/media/beautyblog.22b33353.jpg"
+  },
+  {
+    "revision": "bdb21fa3d3a500194904bb52db7096aa",
+    "url": "/FastTrackPortfolio/static/media/craftcritic.bdb21fa3.jpg"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/FastTrackPortfolio/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "276ca1b6314bef03d5bc0b2eae0c8700",
+    "url": "/FastTrackPortfolio/static/media/electroEnthusiast.276ca1b6.jpeg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/FastTrackPortfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/FastTrackPortfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/FastTrackPortfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/FastTrackPortfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/FastTrackPortfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/FastTrackPortfolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/FastTrackPortfolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/FastTrackPortfolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/FastTrackPortfolio/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "d61e9ca7422fc3ff2685442ad36cfe13",
+    "url": "/FastTrackPortfolio/static/media/profilepic.d61e9ca7.jpeg"
+  },
+  {
+    "revision": "b0af327d478b1aeeae87c98cf12d9842",
+    "url": "/FastTrackPortfolio/static/media/regex.b0af327d.jpg"
+  }
+]);
